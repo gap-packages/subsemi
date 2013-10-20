510ca78fcada3211991761094b6d30f456414a75
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-10-20 14:27:09 +1100

    bruteforce function revised, renamed

--- init.g
+++ init.g
@@ -1,4 +1,5 @@
 ReadPackage("SubSemi","config.g"); #the configuration file
 
+ReadPackage("SubSemi","/lib/bruteforce.gd");
 ReadPackage("SubSemi","/lib/conjugacy.gd");
 ReadPackage("SubSemi","/lib/multab.gd");
---
+++ lib/bruteforce.gd
@@ -0,0 +1,10 @@
+################################################################################
+##
+## SubSemi
+##
+## Brute-force submagma search functions
+##
+## Copyright (C) 2013  Attila Egri-Nagy
+##
+
+DeclareGlobalFunction("BFSubMagmas");
