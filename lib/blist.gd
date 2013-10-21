fed89ba646cf74e2cc3eb66747821842383b8bc7
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-10-21 14:25:09 +1100

    making functions global

---
+++ lib/blist.gd
@@ -0,0 +1,11 @@
+################################################################################
+##
+## SubSemi
+##
+## Extra functions for boolean lists. Indexing, encoding.
+##
+## Copyright (C) 2013  Attila Egri-Nagy
+##
+
+DeclareGlobalFunction("FirstEntryPosOr1");
+DeclareGlobalFunction("LastEntryPosOr1");
