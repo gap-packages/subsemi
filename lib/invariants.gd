a823d84894e8c6bff69e7345459b82a6450db168
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-11-02 09:45:17 +1100

    separating the invariants

---
+++ lib/invariants.gd
@@ -0,0 +1,18 @@
+################################################################################
+##
+## SubSemi
+##
+## Several properties for multiplication tables and their elements.
+## Used for quickly deciding non-isomorphism.
+##
+## Copyright (C) 2013  Attila Egri-Nagy
+##
+
+DeclareGlobalFunction("Frequencies");
+#DeclareGlobalFunction("Frequencies");
+DeclareGlobalFunction("DiagonalPartition");
+DeclareGlobalFunction("MulTabProfile");
+DeclareGlobalFunction("ElementProfile");
+DeclareGlobalFunction("AbstractIndexPeriod");
+DeclareGlobalFunction("RowPartition");
+DeclareGlobalFunction("ColumnPartition");
