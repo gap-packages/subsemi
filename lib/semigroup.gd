7f0862ef9de6eed10afe67dd4a231235055b34c9
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-11-07 13:27:18 +1100

    just moving the 3-nilpotent code

---
+++ lib/semigroup.gd
@@ -0,0 +1,10 @@
+################################################################################
+##
+## SubSemi
+##
+## General functions for semigroups
+##
+## Copyright (C) 2013  Attila Egri-Nagy
+##
+
+DeclareGlobalFunction("Is3NilPotent");
--- read.g
+++ read.g
@@ -1,3 +1,4 @@
+ReadPackage("SubSemi","/lib/semigroup.gi");
 ReadPackage("SubSemi","/lib/ideals.gi");
 ReadPackage("SubSemi","/lib/util.gi");
 ReadPackage("SubSemi","/lib/bruteforce.gi");
