4f8fbedecf54ffc5f135889d622dd6e6199995d5
Attila Egri-Nagy <attila@egri-nagy.hu>
2013-12-19 14:57:42 +1100

    reviewing 1extensions

--- lib/1extensions.gd
+++ lib/1extensions.gd
@@ -1,2 +1,11 @@
+################################################################################
+##
+## SubSemi
+##
+## Enumerating subsemigroups by recursively extending with a generator.
+##
+## Copyright (C) 2013-2014  Attila Egri-Nagy
+##
+
 DeclareGlobalFunction("SubSgpsBy1Extensions");
-DeclareGlobalFunction("SubSgpsBy1ExtensionsWithLimitSet");
+DeclareGlobalFunction("SubSgpsBy1ExtensionsParametrized");
