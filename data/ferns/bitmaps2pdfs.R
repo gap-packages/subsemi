e6c2d017c0566a5b144f0be126da36d0d8e93a24
Attila Egri-Nagy <attila@egri-nagy.hu>
2014-08-26 15:42:43 +1000

    proper regexp

--- data/ferns/bitmaps2pdfs.R
+++ data/ferns/bitmaps2pdfs.R
@@ -1,8 +1,7 @@
-bitmapfiles <- list.files(pattern="*.bitmap")
+bitmapfiles <- list.files(pattern="*[.]bitmap$")
 for(f in bitmapfiles){
-pdf(paste(f,"pdf",sep="."));
-
-      m <- as.matrix(read.table(f));
-      image(m, axes = FALSE, col = grey(seq(0, 1, length = 2)));
-dev.off();
+  m <- as.matrix(read.table(f));
+  pdf(paste(f,"pdf",sep="."));
+  image(m, axes = FALSE, col = grey(seq(0, 1, length = 2)));
+  dev.off();
 }
