82bd4a1c6f002b92efeba12fd2b836a89238ee7f
Attila Egri-Nagy <attila@egri-nagy.hu>
2014-10-21 13:28:45 +1100

    trying to get the enumeration script right

--- data/T4/enumeration.sh
+++ data/T4/enumeration.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-echo "Read(\"T4defs.g\");P_T4();" | gap
+LOADER="Read(\"T4defs.g\");"
+XLOADER="Read(\\\"T4defs.g\\\");" #escape character madness
+#echo $LOADER"P_T4();" | gap
+
+#echo $LOADER"K43modK42subs();" | gap
+#sort -u K43modK42.uppertorsos > K43modK42.suts
+#split -n 128 K43modK42.su
+
+rm UTtasks;
+for i in UT*; do echo "echo \"$XLOADER K43SubsFromUpperTorsos(\\\"$i\\\");\" | gap  -m 8g" >> UTtasks; done; 
+
