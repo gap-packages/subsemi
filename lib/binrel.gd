57649db4a33451c0c5becc50de7b69c1f504daef
Attila Egri-Nagy <attila@egri-nagy.hu>
2014-12-27 10:47:08 +1100

    adding the binary relation stuff

---
+++ lib/binrel.gd
@@ -0,0 +1,10 @@
+################################################################################
+##
+## SubSemi
+##
+## Extra functions for binary relations.
+##
+## Copyright (C) 2013-2014  Attila Egri-Nagy
+##
+
+DeclareGlobalFunction("ConjugateBinaryRelation");
