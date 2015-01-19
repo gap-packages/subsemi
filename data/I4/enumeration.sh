6400535226f4316d137d0c4fe5e58d1ea8800901
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-01-19 20:49:48 +1100

    from the harvester

--- data/I4/enumeration.sh
+++ data/I4/enumeration.sh
@@ -10,20 +10,20 @@ LOADER="Read(\"I4defs.g\");"      #variables and functions needed
 XLOADER="Read(\\\"I4defs.g\\\");" #escape character madness
 
 #subs with nontrivial subgroup + trivial group
-echo $LOADER"P_I4();" | gap
+echo $LOADER"P_I4();" | gap -q -m 30g
 
 #subs of I43 mod I42
-echo $LOADER"I43modI42subs();" | gap -q -m 7g
+echo $LOADER"I43modI42subs();" | gap -q -m 30g
 sort -u I43modI42.uppertorsos > I43modI42.suts
 
 ###### extending the uppertorsos from I43/I42 ##################################
 rm UT*
 split -l 10000 I43modI42.suts UT
 rm UTtasks;
-for i in UT*; do echo "echo \"$XLOADER I43SubsFromUpperTorsos(\\\"$i\\\");\" | gap  -q -m 3g" >> UTtasks; done;
+for i in UT*; do echo "echo \"$XLOADER I43SubsFromUpperTorsos(\\\"$i\\\");\" | gap  -q -m 7g" >> UTtasks; done;
 parallel < UTtasks
 cat UT*M > I43.reps
 
-echo $LOADER"I43sharp();" | gap -10g
+echo $LOADER"I43sharp();" | gap -30g
 
 cat I43_I4.reps I43sharp_I4.reps P_I4.reps > I4.reps
