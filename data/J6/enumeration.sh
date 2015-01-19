44bbc526c71f5b800a0872f4dc8bf62783cf70d6
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-01-19 20:59:35 +1100

    J6 stuff from harvester

--- data/J6/enumeration.sh
+++ data/J6/enumeration.sh
@@ -9,18 +9,20 @@
 export LOADER="Read(\"J6defs.g\");"      #variables and functions needed 
 export XLOADER="Read(\\\"J6defs.g\\\");" #escape character madness
 
-echo $LOADER"I1CmodI2Csubs();" | gap -q -m 2g # fast
-sort -u I1CmodI2C.uppertorsos > I1CmodI2C.suts
-###### extending the uppertorsos from I1C/I2C ##################################
-rm UT*
-split -l 100 I1CmodI2C.suts UT
-rm UTtasks;
-for i in UT*; do echo "echo \"$XLOADER I1CSubsFromUpperTorsos(\\\"$i\\\");\" | gap  -q -m 7g" >> UTtasks; done;
-parallel --joblog I1C.log < UTtasks
-cat UT*M > I1CminusI2C.reps #since it surely contains sg from I1C/I2C;
+echo $LOADER"I3CSubs();" | gap -q # immediate
 
-echo $LOADER"I2CmodI3Csubs();" | gap -q -m 7g -K 14g # 8hours
-sort -u I2CmodI3C.uppertorsos > I2CmodI3C.suts
+#echo $LOADER"I1CmodI2Csubs();" | gap -q -m 2g # fast
+#sort -u I1CmodI2C.uppertorsos > I1CmodI2C.suts
+###### extending the uppertorsos from I1C/I2C ##################################
+#rm UT*
+#split -l 1 I1CmodI2C.suts UT #just do it one by one
+#rm UTtasks;
+#for i in UT*; do echo "echo \"$XLOADER I1CSubsFromUpperTorsos(\\\"$i\\\");\" | gap  -q -m 7g" >> UTtasks; done;
+#parallel --joblog I1C.log < UTtasks
+#cat UT*M > I1CminusI2C.reps #since it surely contains sg from I1C/I2C;
+
+#echo $LOADER"I2CmodI3Csubs();" | gap -q -m 7g -K 14g # 8hours
+#sort -u I2CmodI3C.uppertorsos > I2CmodI3C.suts
 ###### extending the uppertorsos from I2C/I3C ##################################
 rm UT*
 split -l 10000 I2CmodI3C.suts UT
