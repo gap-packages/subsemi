2bb36a6e6bf46e87b0c5fcfb6de55ff5dbcfa38f
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-01-31 16:54:29 +1100

    now doing the classification

--- data/T4/classification.sh
+++ data/T4/classification.sh
@@ -4,25 +4,15 @@
 # Classifying all subsemigroups of T4 ##########################################
 ################################################################################
 # INPUT: T4.reps
-# OUTPUT: T4.reps - a file containing all subs in 6-packed encoding
+# OUTPUT: 
+
+CORES="16" #number of cores
 
 LOADER="Read(\"T4defs.g\");"      #variables and functions needed 
 XLOADER="Read(\\\"T4defs.g\\\");" #escape character madness
 
-# in gaprc put SemigroupsOptionsRec.hashlen:=NextPrimeInt(2*256); to save memory
+echo "FilingIndicatorFunctionsBySize(\"T4.reps\",3);" | gap -q
 
-<<<<<<< local
-rm X*
-split -l 100000 T4.reps X
-rm Xtasks;
-=======
-#subs of K43 mod K42
-echo $LOADER"K43modK42subs();" | gap -q -m 7g
-sort -u K43modK42.uppertorsos > K43modK42.suts
->>>>>>> other
-
-for i in X*; do echo "echo \"$XLOADER IndicatorSetsTOClassifiedSmallGenSet(LoadIndicatorSets(\\\"$i\\\"),MulTab(T4),\\\"T4_\\\",3);\" | gap  -q -m 6g" >> Xtasks; done;
-parallel --joblog X.log < Xtasks
-
-
+for i in S*; do echo "echo \"$XLOADER FilingIndicatorFunctionsBySgpTag(\\\"$i\\\", mtT4, \\\"T4_\\\", 3);\" | gap  -q " >> tasksS; done;
+parallel --jobs $CORES --job-log T4classifying.log < tasksS
 
