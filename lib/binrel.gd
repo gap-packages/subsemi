f2a9d257574da6fac94f86b8921bd5adb34d7474
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-08-14 14:25:54 +1000

    getting the partitioned binary relation stuff from the semigroups' branch

---
+++ lib/binrel.gd
@@ -0,0 +1,14 @@
+############################################################################
+##
+#W  binrel.gi
+#Y  Copyright (C) 2015                                   Attila Egri-Nagy
+##
+##  Licensing information can be found in the README file of this package.
+##
+#############################################################################
+##
+## Extra functions for binary relations. (probably should go to GAP lib)
+##
+
+DeclareGlobalFunction("ConjugateBinaryRelation");
+DeclareGlobalFunction("UnionOfBinaryRelations");
