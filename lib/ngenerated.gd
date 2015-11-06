5de76ad3728f8b6bd1c7f404912ec143767bb781
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-11-06 15:07:04 +1100

    making functions read-only

--- lib/ngenerated.gd
+++ lib/ngenerated.gd
@@ -1,1 +1,10 @@
+################################################################################
+##
+## SubSemi
+##
+## Enumerating subsemigroups by minimal generating sets. TODO obsolete?
+##
+## Copyright (C) 2013-2015  Attila Egri-Nagy
+##
+
 DeclareGlobalFunction("SubSgpsByMinimalGenSets");
