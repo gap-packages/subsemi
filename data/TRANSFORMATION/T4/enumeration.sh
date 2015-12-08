fb2fbd4d4cc2831bea65b98e217c1e654fa3c6d1
Attila Egri-Nagy <attila@egri-nagy.hu>
2015-12-08 15:14:13 +1100

    checking enumeration script

--- data/TRANSFORMATION/T4/enumeration.sh
+++ data/TRANSFORMATION/T4/enumeration.sh
@@ -10,14 +10,14 @@ MAXMEM="15g"  #for single process
 CORES="4" #number of cores
 SMALLMEM="3800m" #for parallel processing
 
-LOADER="Read(\"T4defs.g\");"      #variables and functions needed 
+LOADER="Read(\"T4defs.g\");"      #variables and functions needed
 XLOADER="Read(\\\"T4defs.g\\\");" #escape character madness
 
 #subs with nontrivial subgroup + trivial group
 echo $LOADER"P_T4();" | gap -q -m $MAXMEM
 
 #subs of K42
-echo $LOADER"K42SubReps;" | gap -q -m $MAXMEM
+echo $LOADER"K42SubReps();" | gap -q -m $MAXMEM
 
 #subs of K43 mod K42
 echo $LOADER"K43modK42subs();" | gap -q -m $MAXMEM
@@ -30,7 +30,7 @@ rm UTtasks;
 for i in UT*; do echo "echo \"$XLOADER K43SubsFromUpperTorsos(\\\"$i\\\");\" | gap  -q -m $SMALLMEM" >> UTtasks; done;
 parallel --jobs $CORES --job-log K43modK42.log < UTtasks
 cat K42_K43.reps > K43.reps
-for i in  UT*M; do cat $i >> K43.reps; done; 
+for i in  UT*M; do cat $i >> K43.reps; done;
 
 echo $LOADER"K43sharp();" | gap -q -m $MAXMEM
 
