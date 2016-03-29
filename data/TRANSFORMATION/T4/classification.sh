59546968c1fd331da424d50f1b2e84fa42af750f
Attila Egri-Nagy <attila@egri-nagy.hu>
2016-03-29 15:14:54 +0900

    classification script finished

--- data/TRANSFORMATION/T4/classification.sh
+++ data/TRANSFORMATION/T4/classification.sh
@@ -3,16 +3,23 @@
 ################################################################################
 # Classifying all subsemigroups of T4 ##########################################
 ################################################################################
-# INPUT: T4.reps
-# OUTPUT: 
+# INPUT: T4.subs
+# OUTPUT: T4.db
 
 CORES="4" #number of cores
+MEM="3800m"
+CHUNKSIZE=10000
 
-LOADER="Read(\"T4defs.g\");"      #variables and functions needed 
+LOADER="Read(\"T4defs.g\");"      #variables and functions needed
 XLOADER="Read(\\\"T4defs.g\\\");" #escape character madness
 
-echo "FilingIndicatorFunctionsBySize(\"T4.reps\",3);" | gap -q
-
-for i in S*; do echo "echo \"$XLOADER FilingIndicatorFunctionsBySgpTag(\\\"$i\\\", MulTab(T4,S4), \\\"T4_\\\", 3);\" | gap  -q " >> tasksS; done;
-parallel --jobs $CORES --joblog T4classifying.log < tasksS
-
+if [ ! -f T4.db ]; then
+    find . -name 'SUBS*' -delete
+    split -l $CHUNKSIZE T4.subs SUBS
+    for i in `ls -1 SUBS*`; do
+        echo "echo \"$XLOADER SgpsDatabase(\\\""$i"\\\", MulTab(T4,S4));\" \
+              | gap -m $MEM" >> SUBStasks;
+    done;
+    parallel --jobs $CORES --joblog T4classifying.log < SUBStasks
+    cat SUBS*.db > T4.db
+fi;
