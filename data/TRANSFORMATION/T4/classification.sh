4b331e8f9c0d12bf45e81878dc588cdf0f0e8b15
Attila Egri-Nagy <attila@egri-nagy.hu>
2016-05-05 12:15:20 +0900

    commenting

--- data/TRANSFORMATION/T4/classification.sh
+++ data/TRANSFORMATION/T4/classification.sh
@@ -14,12 +14,17 @@ LOADER="Read(\"T4defs.g\");"      #variables and functions needed
 XLOADER="Read(\\\"T4defs.g\\\");" #escape character madness
 
 if [ ! -f T4.db ]; then
+    #removing any leftover chunks
     find . -name 'SUBS*' -delete
+    #splitting the big file into chunks
     split -l $CHUNKSIZE T4.subs SUBS
+    #creating a script for each chunk
     for i in `ls -1 SUBS*`; do
         echo "echo \"$XLOADER SgpsDatabase(\\\""$i"\\\", MulTab(T4,S4));\" \
               | gap -m $MEM" >> SUBStasks;
     done;
+    #processing the chunks with parallel
     parallel --jobs $CORES --joblog T4classifying.log < SUBStasks
+    #merging the the chunk databases into a single file
     cat SUBS*.db > T4.db
 fi;
